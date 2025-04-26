419969b8febaf425abf1edc3030a780226213cb0
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-04-26 23:32:28 +0700

    menambahkan 7 app baru

--- tugas_kelompok_basdat/settings.py
+++ tugas_kelompok_basdat/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'animals',
+    'habitats',
+    'medical',
+    'feeding',
+    'attractions',
+    'tickets',
+    'adoption',
 ]
 
 MIDDLEWARE = [
